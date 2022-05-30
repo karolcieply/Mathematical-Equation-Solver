25c3a905ee91d05bb63ae51385784c97c1f33d68
karolcieply <72377467+karolcieply@users.noreply.github.com>
2022-05-30 22:05:33 +0200

    ADD: data processing class

--- solver/model/solverModel.py
+++ solver/model/solverModel.py
@@ -6,18 +6,46 @@ from PIL import Image
 from typing import List, Tuple
 from sklearn.neighbors import KNeighborsClassifier
 import matplotlib.pyplot as plt
-
+import pandas as pd
 MODEL_PATH = "solver/model/solverModel.pkl"
-
 logging.basicConfig(level=logging.INFO)
+
+
+class DataProcessing:
+    @staticmethod
+    def prepareTrainTestSet() -> Tuple[List[List[float]], list[str]]:
+        from tensorflow.keras.datasets import mnist
+        (X_train, y_train), (X_test, y_test) = mnist.load_data()
+        X_train = X_train/255
+        X_train_reshaped = X_train.reshape(len(X_train), 28*28)
+        X_test = X_test/255
+        X_test_reshaped = X_test.reshape(len(X_test), 28*28)
+        return X_train_reshaped, y_train, X_test_reshaped, y_test
+    
+    @staticmethod
+    def compressImage(image: np.array):
+        input_size = 280
+        output_size = 28
+        bin_size = input_size // output_size
+        small_image = np.asarray(image).reshape((output_size, bin_size,
+                                                 output_size, bin_size, 1)
+                                                ).min(3).min(1)
+        return ((np.asarray(small_image)*-1+255)/255).reshape(1, 28*28)
+
+    @staticmethod
+    def plotImage(image: np.ndarray) -> None:
+        fig = plt.figure()
+        plt.imshow(image, cmap='gray')
+        plt.show()
+    
 
 class SolverModel:
     __fitted: bool = False
     __created: bool = False
     __clf: KNeighborsClassifier = None
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, k: int = 5, p: int = 2) -> None:
+        self.createModel(k, p)
 
     def loadModel(self) -> None:
         with open(MODEL_PATH, "rb") as f:
@@ -33,9 +61,9 @@ class SolverModel:
         else:
             raise Exception("Model not created or not fitted")
 
-    def createModel(self, k: int = 5, knnP: int = 2) -> None:
+    def createModel(self, k: int = 5, p: int = 2) -> None:
         self.__created = True
-        self.__clf = KNeighborsClassifier(n_neighbors=k, p=knnP)
+        self.__clf = KNeighborsClassifier(n_neighbors=k, p=p)
         logging.info("Model Created")
 
     def predict(self, data: List[float]) -> str:
@@ -43,44 +71,27 @@ class SolverModel:
             raise Exception("Model not fitted")
         return self.__clf.predict(data)
     
-    def fitModel(self) -> None:
+    def fitModel(self, trainTestTuple: tuple = None) -> None:
         if not self.__created: 
             raise Exception("Model not created")
         if self.__fitted:
             raise Exception("Model already fitted")
-        X_train, y_train, X_test, y_test = SolverModel.prepareTrainSet()
+        if trainTestTuple is None:
+            X_train, y_train, X_test, y_test =\
+                DataProcessing.prepareTrainTestSet()
+        else:
+            X_train, y_train, X_test, y_test = trainTestTuple
         self.__clf.fit(X_train, y_train)
         logging.info("Model fitted")
         self.__fitted = True
         logging.info(f"Model Score: {self.__clf.score(X_test, y_test)}")
 
-    @staticmethod
-    def prepareTrainSet() -> Tuple[List[List[float]], list[str]]:
-        from tensorflow.keras.datasets import mnist
-        (X_train, y_train), (X_test, y_test) = mnist.load_data()
-        X_train = X_train/255
-        X_train_reshaped = X_train.reshape(len(X_train), 28*28)
-        X_test = X_test/255
-        X_test_reshaped = X_test.reshape(len(X_test), 28*28)
-        return X_train_reshaped, y_train, X_test_reshaped, y_test
-    
     def predictUploadedImage(self, imagePath) -> int:
         with open(imagePath, "rb") as f:
             readBytes = f.read()
         img = Image.open(io.BytesIO(readBytes)).convert('L')
-        input_size = 280
-        output_size = 28
-        bin_size = input_size // output_size
-        small_image = np.asarray(img).reshape((output_size, bin_size, output_size, bin_size, 1)).min(3).min(1)
-        topredict = ((np.asarray(small_image)*-1+255)/255).reshape(1, 28*28)
-        image = small_image
-        return self.__clf.predict(topredict)[0]
+        return self.__clf.predict(DataProcessing.compressImage(img))[0]
 
-    @staticmethod
-    def plotImage(image: np.ndarray) -> None:
-        fig = plt.figure()
-        plt.imshow(image,cmap='gray')
-        plt.show()
         
 # def main():
 #     sm = SolverModel()
